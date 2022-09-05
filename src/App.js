0871367dc72c9d074a4fe4b463e6e62fa42da58d
starstormfall <photon_dazzles_02@icloud.com>
2022-09-05 14:30:57 +0800

    reset frontend for bigfoot sql

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React from "react";
+import logo from "./logo.png";
 import "./App.css";
-
-import { Link, Outlet } from "react-router-dom";
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <h1> 3.E.1: Bigfoot JSON </h1>
-        <nav
-          style={{
-            borderBottom: "solid 1px",
-            paddingBottom: "1rem",
-            display: "flex",
-            justifyContent: "space-around",
-          }}
-        >
-          <Link to="/">Home</Link>
-          <Link to="/sightings">Sightings</Link>
-        </nav>
-        <Outlet />
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+        </header>
       </div>
     );
   }
